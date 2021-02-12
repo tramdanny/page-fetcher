27b8ba936048663e9e4d558cb84daf6a43589ec5
Danny <tramdanny@hotmail.com>
2021-02-12 00:28:27 +0000

    done page fetcher

--- fetcher.js
+++ fetcher.js
@@ -1,8 +1,17 @@
 const request = require('request');
 const fs = require('fs');
 
-request('http://www.google.com', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+let args = process.argv.slice(2);
+
+
+
+request(args[0], function (error, response, body) {
+  if(error) {
+    console.log(error);
+    return;
+  }
+  fs.writeFile(args[1], body, function (err) {
+    if (err) throw err;
+  });
+  console.log("Downloaded and saved " + body.length + " bytes to " + args[1]);
 });
